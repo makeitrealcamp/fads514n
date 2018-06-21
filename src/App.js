1b7e1560cc7be343ec14af490aa3b7d778daba00
Yebrail Andres Pacheco Jaime <yebrail.pacheco@globant.com>
2018-06-21 10:41:40 -0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Mostrar una lista en base a un arreglo
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      list: ['Javascript', 'CSS', 'HTLM', 'React']
-    }
+      list: ["Javascript", "CSS", "HTLM", "React"]
+    };
   }
+
   render() {
+
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
-        <ul style={styles.list}>
-          {/* Tu codigo va aca */}
-        </ul>
+        <ul style={styles.list}>{this.state.list.map( info =>(
+          <li>{info}</li>
+        ))}</ul>
       </div>
     );
   }
@@ -23,19 +25,19 @@ class App extends Component {
 
 const styles = {
   container: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
   },
   title: {
-    marginTop: '100px'
+    marginTop: "100px"
   },
   list: {
-    fontSize: '20px',
+    fontSize: "20px",
     flexGrow: 6
   }
-}
+};
 
 export default App;
